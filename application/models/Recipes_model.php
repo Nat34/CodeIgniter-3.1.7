3cb7b6fc2dcfe7d487756035ad0084b7f80bc93e
Nat34 <nathan.a.thomas@gmail.com>
2018-04-21 15:21:55 -0500

    Spacing / Comment out remove method

--- application/models/Recipes_model.php
+++ application/models/Recipes_model.php
@@ -1,29 +1,27 @@
 <?php
 class Recipes_model extends CI_Model {
 
-        public function __construct()
-        {
-                $this->load->database();
-        }
-        
-        public function get_recipes($slug = FALSE)
-	{
-	        if ($slug === FALSE)
-	        {
-	                $query = $this->db->get('recipes');
-	                return $query->result_array();
-	        }
-	
-	        $query = $this->db->get_where('recipes', array('slug' => $slug));
-	        return $query->row_array();
-	}
-	
+  public function __construct()
+  {
+          $this->load->database();
+  }
+
+  public function get_recipes($slug = FALSE)
+  {
+    if ($slug === FALSE) {
+      $query = $this->db->get('recipes');
+      return $query->result_array();
+    }
+      $query = $this->db->get_where('recipes', array('slug' => $slug));
+      return $query->row_array();
+   }
+
 	public function set_recipes()
 	{
 	    $this->load->helper('url');
-	
+
 	    $slug = url_title($this->input->post('title'), 'dash', TRUE);
-	    
+
 	    $recipe_data = array(
 	    	'source'       => $this->input->post('source', TRUE),
 	        'title'        => $this->input->post('title', TRUE),
@@ -157,48 +155,49 @@ class Recipes_model extends CI_Model {
 		{
 			return;
 		}
-		
+
 		$query = $this->db->query("SELECT `instructions` FROM `instructions_icing` WHERE `source` = '".$source_id."'");
 		return $query->result_array();
 	}
-
+/*
 	public function remove_recipe($slug)
 	{
 		$query = $this->db->query(
-			"SELECT `id` 
-			FROM  `recipes` 
+			"SELECT `id`
+			FROM  `recipes`
 			WHERE  `slug` =  '".$slug."'"
 			);
 		$row = $query->row();
-		
+
 		if (isset($row))
 		{
 		   $query = $this->db->query(
-			"DELETE 
-			FROM  `recipes` 
+			"DELETE
+			FROM  `recipes`
 			WHERE  `id` =  '".$row->id."'"
 			);
-			
+
 		   $query = $this->db->query(
-			"DELETE 
-			FROM  `ingredients` 
+			"DELETE
+			FROM  `ingredients`
 			WHERE  `source` =  '".$row->id."'"
 			);
 
 		   $query = $this->db->query(
-			"DELETE 
-			FROM  `ingredients_icing` 
+			"DELETE
+			FROM  `ingredients_icing`
 			WHERE  `source` =  '".$row->id."'"
 			);
 
 		   $query = $this->db->query(
-			"DELETE 
-			FROM  `instructions_icing` 
+			"DELETE
+			FROM  `instructions_icing`
 			WHERE  `source` =  '".$row->id."'"
 			);
 
 		}
 
 	}
-	
+  */
+
 }
