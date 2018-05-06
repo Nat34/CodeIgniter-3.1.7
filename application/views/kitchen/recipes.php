0c89ff5622af3351175c223615f5c227ea0c2198
Nat34 <nathan.a.thomas@gmail.com>
2018-05-06 15:01:41 -0500

    add select box to recipes view and add sorting script

--- application/views/kitchen/recipes.php
+++ application/views/kitchen/recipes.php
@@ -1,12 +1,20 @@
 <div class="form column-wrapper clearfix">
 	<?php /* <div class="cp-title"><?php echo $title; ?></div> */ ?>
-	<div class="searchbox">
-		<label for="search-by"><i class="fa fa-search"></i></label>
-		<input id="search-by" type="text" placeholder="Search Recipes">
+	<div class="selectbox_wrapper">
+		<select class="selectbox" name="Category">
+		<option value="select">Select a category</option>
+		<option value="all">All</option>
+		<option value="cookies">Cookies</option>
+		<option value="cakes">Cakes</option>
+		<option value="chicken">Chicken</option>
+		<option value="beef">Beef</option>
+		<option value="seafood">Seafood</option>
+		<option value="breakfast_items">Breakfast Items</option>
+		</select>
 	</div>
 		<div class="recipes">
 		<?php foreach ($recipes as $recipes_item): ?>
-		<div class="recipe">
+		<div class="recipe <?php echo $recipes_item['category']; ?>">
 		        <h3><?php echo $recipes_item['title']; ?></h3>
 		                <div class="description"><?php echo $recipes_item['description']; ?></div>
 		        <div class="recipe_view"><a href="<?php echo site_url('recipes/'.$recipes_item['slug']); ?>">View recipe</a></div>
@@ -14,3 +22,20 @@
 		<?php endforeach; ?>
 		</div>
 </div>
+
+<script type="text/javascript">
+$(".selectbox").change( function(){
+let category = $(this).val();
+const recipes = $(".recipes");
+    recipes.find(".recipe").each(function(index, value){
+    	if (!$(value).hasClass(category)) {
+				$(value).addClass("hidden");
+        } else {
+            $(value).removeClass("hidden");
+        }
+        if (category == "all" || category == "select") {
+            $(value).removeClass("hidden");
+        }
+    });
+});
+</script>
